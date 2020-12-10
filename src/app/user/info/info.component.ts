ef162b4e09094fd61a1e45e8662d8325646285bf
Adrian Richter <adrichter97@gmail.com>
2020-12-10 18:35:01 +0100

    user-info component uses backend info now

--- src/app/user/info/info.component.ts
+++ src/app/user/info/info.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { InfoService, InfoData } from 'src/app/info/info.service';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css']
+  styleUrls: ['./info.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class UserInfoComponent implements OnInit {
 
