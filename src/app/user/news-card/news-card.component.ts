64b6a6edc7af1aef5b7469dec7e1ac3469017fe2
Adrian Richter <adrichter97@gmail.com>
2020-12-14 18:28:28 +0100

    user/news-card can now display embedded html generated by ckeditor

--- src/app/user/news-card/news-card.component.ts
+++ src/app/user/news-card/news-card.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { NewsData } from 'src/app/news/news.service';
 
 @Component({
   selector: 'app-user-news-card',
   templateUrl: './news-card.component.html',
-  styleUrls: ['./news-card.component.css']
+  styleUrls: ['./news-card.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class UserNewsCardComponent implements OnInit {
 
