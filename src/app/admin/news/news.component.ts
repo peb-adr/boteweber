30efd0d9d2f05f0d40e1dbe1c9ce8ccf743023c8
Adrian Richter <adrichter97@gmail.com>
2020-12-14 17:20:00 +0100

    admin news component now loads and displays from backend

--- src/app/admin/news/news.component.ts
+++ src/app/admin/news/news.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { NewsData, NewsService } from 'src/app/news/news.service';
 
 @Component({
   selector: 'app-admin-news',
@@ -7,9 +9,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminNewsComponent implements OnInit {
 
-  constructor() { }
+  editors_title: ClassicEditor[] = [];
+  editors_message: ClassicEditor[] = [];
+  
+  news: NewsData[];
+
+  constructor(private newsService: NewsService) { }
 
   ngOnInit(): void {
+    this.newsService.getNews().toPromise()
+    .then((data: NewsData[]) => {
+      this.news = data
+    })
+    .then(() => {
+      return this.createEditors()
+    })
+    .then(() => {
+      this.loadNews();
+    });
   }
 
+  loadNews() {
+    for (let i = 0; i < this.news.length; i++) {
+      this.editors_title[i].setData(this.news[i].title);
+      this.editors_message[i].setData(this.news[i].message);
+    }
+  }
+
+  createEditors() {
+    let config = {
+      toolbar: [
+        'heading', '|',
+        'bold', 'italic', 'link', 'bulletedList', 'numberedList', '|',
+        'imageUpload', 'blockQuote', 'mediaEmbed', '|',
+        'undo', 'redo'
+      ]
+    }
+
+    let promises = [];
+    
+    for (const n of this.news) {
+      promises.push(ClassicEditor
+        .create("<p>empty title</p", config)
+        .then(editor => {
+          document.querySelector("#editor-container").appendChild(editor.ui.element);
+          this.editors_title.push(editor);
+          console.log(this.editors_title);
+        })
+        // .catch(error => {
+        //   // console.error(error);
+        // });
+      )
+
+      promises.push(ClassicEditor
+        .create("<p>empty message</p>", config)
+        .then(editor => {
+          document.querySelector("#editor-container").appendChild(editor.ui.element);
+          this.editors_message.push(editor);
+          console.log(this.editors_message);
+        })
+        // .catch(error => {
+        //   // console.error(error);
+        // });
+      )
+    }
+
+    return Promise.all(promises);
+  }
+  
+  onClickStore() {
+    // console.log(this.info.text);
+    // this.storeInfo();
+  }
+
+  onClickReset() {
+    // console.log(this.info.text);
+    // this.resetInfo();
+    this.loadNews();
+  }
+  
 }
