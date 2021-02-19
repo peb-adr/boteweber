6441a4e748e4616085f450c25bb06e2c1b1efd35
Adrian Richter <adrichter97@gmail.com>
2021-02-19 17:44:31 +0100

    small topbar adjustments

--- src/app/admin/topbar/topbar.component.ts
+++ src/app/admin/topbar/topbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-topbar',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminTopbarComponent implements OnInit {
 
-  constructor() { }
+  router: Router;
+  
+  constructor(private iRouter: Router) {
+    this.router = iRouter;
+  }
 
   ngOnInit(): void {
   }
